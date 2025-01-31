d9f111fef003289dc902f45520e65ecea5638922
Sonish2319 <sonish2319@gmail.com>
2025-01-31 09:25:01 +0545

    Your commit message here

--- backend/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router=express.Router();
-
-const checkApiKey =require('../middleware/checkApiKey');
-
-router.get('/test', checkApiKey,(req, res) => {
-    res.json({ message: 'Hello, world!' });
-});
-module.exports = router;
