fd4db3e9d0fe76e19712977d92d4f436bd040c4d
AkantaRaii <ekantarai10@gmail.com>
2025-02-07 15:03:20 +0545

    better ui

--- backend/models/db.js
+++ backend/models/db.js
@@ -4,6 +4,7 @@ const mysql= require('mysql2');
 // Create a connection to the database
 const db=mysql.createConnection({
     host: process.env.DB_HOST,
+    port: 3307,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
