094472e7eeb07c675aab542ca6a2d58f8c4bc8d8
AkantaRaii <ekantarai10@gmail.com>
2025-03-02 22:30:58 +0545

    create api

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const router=express.Router();
 const checkApiKey=require('../middleware/checkApiKey');
+const {getCourse}=require('../controller/api/apiCoursesController');
+const { getSubjects } = require('../controller/api/apiSubjectsController');
+const { getQuestions } = require('../controller/api/apiQuestionController');
+const { getOptions } = require('../controller/api/apiOptionController');
+const { route } = require('./admin');
 
-
+router.get('/courses',getCourse);
+router.get('/course/:courseId/subjects/',getSubjects);
+router.get('/course/subject/:subjectId/question/:limit',getQuestions);
+router.get('/course/subject/question/option/:questionId',getOptions);
 module.exports=router;
