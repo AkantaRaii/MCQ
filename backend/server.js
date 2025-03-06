bb7ff13a905f0db5b12c072cd7c4f0f00f5c05cc
AkantaRaii <ekantarai10@gmail.com>
2025-03-06 23:25:42 +0545

    add ssl for https:

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
 
 const app = express();
 const port = process.env.PORT || 4001;
 
+const options = {
+    key: fs.readFileSync('./ssl/server.key'),
+    cert: fs.readFileSync('./ssl/server.cert')
+};
 // Set EJS as the template engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); // 
@@ -22,6 +29,6 @@ const adminRoutes = require('./routes/admin');
 app.use('/api', apiRoutes);
 app.use('/admin', adminRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+https.createServer(options, app).listen(port, () => {
+    console.log('Server is running on https://localhost:' + port);
 });
